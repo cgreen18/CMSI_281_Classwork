9ff2980f5ba1e053c26de1a68d826b52daca41d7
cgreen18 <35274689+cgreen18@users.noreply.github.com>
2018-09-11 18:26:07 -0700

    first part of ForneymonCardTest.java

---
+++ ForneymonCardTest.java
@@ -0,0 +1,14 @@
+//package forneymon.cardgame;
+
+public class ForneymonCardTest{
+
+    public static void main(String[] args){
+        FlippingForneymonCard burnes = new FlippingForneymonCard(true,"burnes","Burneymon");
+        System.out.println(burnes);
+        burnes.flip();
+        System.out.println(burnes);
+
+
+
+    }
+}
