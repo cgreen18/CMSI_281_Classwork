aacf3489f0ded7c270159bcfa09083dcbebfb9ca
cgreen18 <35274689+cgreen18@users.noreply.github.com>
2018-09-11 18:28:56 -0700

    Final ForneymonCardTest.java

--- ForneymonCardTest.java
+++ ForneymonCardTest.java
@@ -3,12 +3,18 @@
 public class ForneymonCardTest{
 
     public static void main(String[] args){
-        FlippingForneymonCard burnes = new FlippingForneymonCard(true,"burnes","Burneymon");
-        System.out.println(burnes);
-        burnes.flip();
-        System.out.println(burnes);
+        FlippingForneymonCard burny = new FlippingForneymonCard(true,"burnes","Burneymon");
+        System.out.println(burny);
+        burny.flip();
+        System.out.println(burny);
 
-
+        FlippingForneymonCard missingNu = new FlippingForneymonCard();
+        System.out.println(missingNu);
+        missingNu.flip();
+        System.out.println(missingNu);
+        System.out.println(burny.match(missingNu));
+        burny.flip();
+        System.out.println(burny.match(missingNu));
 
     }
 }
